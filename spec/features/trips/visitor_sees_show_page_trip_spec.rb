ae868281b4d40102d4e170790fd01deed8f9e5e5
ClaireBvs <clairembeauvais@gmail.com>
2018-07-27 10:56:38 -0600

    add method to link to trail show page

--- spec/features/trips/visitor_sees_show_page_trip_spec.rb
+++ spec/features/trips/visitor_sees_show_page_trip_spec.rb
@@ -57,8 +57,26 @@ describe 'A visitor' do
 
       expect(page).to have_content("Shortest distance : #{expected_result}")
     end
+
+    it 'can click on the name of a trail
+    to see a show page for that specific trail' do
+    trip = Trip.create(name: 'mountain', start_date: '02/03/2018', end_date: '04/05/2018')
+    trail_1 = trip.trails.create(length: 2, name: 'high pic', address: '32 road')
+    trail_2 = trip.trails.create(length: 4, name: 'low pic', address: '34 trail')
+
+    visit trip_path(trip)
+
+    expect(page).to have_content(trail_1.name)
+    expect(page).to have_content(trail_2.name)
+
+    within "#trail-#{trail_1.id}" do
+      click_link "#{trail_1.name}"
+      expect(current_path).to eq(trail_path(trail_1))
+    end
   end
 end
+end 
+
 
 =begin
 
