5d54cd9cbf551f1221240ceb0be0009a316d7fdf
ClaireBvs <clairembeauvais@gmail.com>
2018-07-27 10:14:41 -0600

    add feature test for trip index page

---
+++ spec/features/trips/visitor_sees_trips_index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'A visitor' do
+  describe 'visiting index trip page' do
+    it 'sees all hiking trips with names and can click on it to visit show page' do
+      trip_1 = Trip.create(name: 'mountain', start_date: '02/03/2018', end_date: '04/05/2018')
+      trip_2 = Trip.create(name: 'mountain', start_date: '02/03/2018', end_date: '04/05/2018')
+
+      visit trips_path
+
+      expect(page).to have_content(trip_1.name)
+      expect(page).to have_content(trip_2.name)
+
+      within "#trip-#{trip_1.id}" do
+        click_link "#{trip_1.name}"
+        expect(current_path).to eq(trip_path(trip_1))
+      end
+    end
+  end
+end
+
+=begin
+As a visitor,
+when I visit the hiking trips index page,
+I see a list of all hiking trip names in
+the database.
+As a visitor,
+when I visit the hiking trips index page,
+and I click on the name of a hiking trip,
+I see a unique page for that hiking trip.
+=end
